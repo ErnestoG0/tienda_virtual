faa78903c49a4dd66fe94b97c4244e686bfe555f
ImNotaTurtle <juniorpacheco5000@gmail.com>
2020-11-01 19:22:36 -0700

    heredasmo de controller func construct home

---
+++ Controllers/Home.php
@@ -0,0 +1,16 @@
+ <?php
+    class Home extends Controllers{
+        public function __construct()
+        {
+
+        }
+        public function home($parems)
+        {
+            echo "Mensaje desde el controlador";
+        }
+        public function($spares)
+        {
+            echo "Datos recibidos:",$params;
+        }
+    }
+?>
