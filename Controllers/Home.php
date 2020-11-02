75a15dda87134b45abe93f0dcc8a428951856c44
ImNotaTurtle <juniorpacheco5000@gmail.com>
2020-11-01 19:51:32 -0700

    func datos

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -6,11 +6,17 @@
         }
         public function home($parems)
         {
-            echo "Mensaje desde el controlador";
+        //echo "Mensaje desde el controlador";
         }
-        public function($spares)
+        public function datos($spares)
         {
             echo "Datos recibidos:",$params;
         }
+        public function carrito($spares)
+        {
+            $carrito = $this ->model ->getCarrito($params);
+            echo $carrito;
+        }
+
     }
 ?>
