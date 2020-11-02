cea40c718700d31c6f5869a8b4fa7da431036106
ImNotaTurtle <juniorpacheco5000@gmail.com>
2020-11-01 19:44:57 -0700

    parent dentro de func construct

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -2,7 +2,7 @@
     class Home extends Controllers{
         public function __construct()
         {
-
+            parent::__construct();
         }
         public function home($parems)
         {
